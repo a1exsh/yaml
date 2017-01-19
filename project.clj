bbaca516a08dcf1a40cb0ca04cbbe6480ee2ff16
Oleksandr Shulgin <oleksandr.shulgin@zalando.de>
2017-01-19 10:17:46 +0100

    Use org.yaml/snakeyaml-1.18-SNAPSHOT / pull in base64 whitespace fix

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject io.forward/yaml "1.0.5"
+(defproject io.forward/yaml "1.0.6-SNAPSHOT"
   :description "A YAML library for Clojure"
   :url "http://github.com/owainlewis/yaml"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.yaml/snakeyaml "1.17"]
-                 [org.flatland/ordered "1.5.2"]])
+                 [org.yaml/snakeyaml "1.18-SNAPSHOT"]
+                 [org.flatland/ordered "1.5.2"]]
+  :repositories  [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots"]])
